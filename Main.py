d8fa930683d478e662e00ada7c59ac54539a9ae5
ash-77696e <haridas.ashwin@gmail.com>
2021-01-29 00:13:07 -0500

    added command line args reading

--- Main.py
+++ Main.py
@@ -10,6 +10,8 @@ range_set = np.array([-0.5, 0.5, 2.5, 4.5])
 def main():
     dimension = int(sys.argv[1])
     density = float(sys.argv[2])
+    algorithm = sys.argv[3]
+    print(algorithm)
     cmap = colors.ListedColormap(color_set)
     norm = colors.BoundaryNorm(range_set, len(color_set))
     plt.figure(figsize = (8, 8))
@@ -19,7 +21,7 @@ def main():
     plt.show()
 
 def generate_maze(dimension, density):
-    maze = np.zeros((dimension, dimension), float)
+    maze = np.zeros((dimension, dimension), int)
     maze[0][0] = 3 # start
     maze[dimension - 1][dimension - 1] = 4 # goal
 
