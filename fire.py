3a0f1493a46a601d2c078843bce32d37cde9733f
ash-77696e <haridas.ashwin@gmail.com>
2021-02-19 16:48:12 -0500

    testing done

--- fire.py
+++ fire.py
@@ -43,14 +43,14 @@ def init_fire(maze):
 
     print(str(randX) + ','  + str(randY))
 
-    return fire_maze
+    return fire_maze, (randX, randY)
 
 def strategy_one(maze, fire, start, goal, q):
     fire_maze = np.copy(fire)
     status, traversal_path, num_explored_nodes = bfs(fire_maze, start, goal)
+
     if status == 'No Solution':
-        print('No path possible')
-        return fire_maze
+        return 'No Path Possible', fire_maze
 
     dimension = maze.shape[0]
 
@@ -59,8 +59,7 @@ def strategy_one(maze, fire, start, goal, q):
 
     while fire_maze[curX][curY] != 7:
         if fire_maze[curX][curY] == 4:
-            print('Escaped')
-            return fire_maze
+            return 'Escaped', fire_maze
 
         if fire_maze[curX][curY] != 3:
             fire_maze[curX][curY] = 6
@@ -74,10 +73,9 @@ def strategy_one(maze, fire, start, goal, q):
         elif curY - 1 >= 0 and (traversal_path[curX][curY - 1] == 6 or traversal_path[curX][curY - 1] == 4) and fire_maze[curX][curY - 1] != 6:
             curY = curY - 1
 
-        fire_maze = advance_fire_one_step(fire_maze, 0.2)
+        fire_maze = advance_fire_one_step(fire_maze, q)
         
-    print('Died')
-    return fire_maze
+    return 'Died', fire_maze
 
 def strategy_two(maze, fire, start, goal, q):
     fire_maze = np.copy(fire)
@@ -91,14 +89,12 @@ def strategy_two(maze, fire, start, goal, q):
     while fire_maze[curX][curY] != 4: # goal
         
         if fire_maze[curX][curY] == 7:
-            print('Died')
-            return fire_maze
+            return 'Died', fire_maze
 
         status, traversal_path, num_explored_nodes = bfs(temp_maze, temp_start, goal) 
 
         if status == 'No Solution':
-            print('No path possible')
-            return fire_maze
+            return 'No Path Possible', fire_maze
         
         if curX + 1 < dimension and (traversal_path[curX + 1][curY] == 6 or traversal_path[curX + 1][curY] == 4):
             curX = curX + 1
@@ -116,80 +112,10 @@ def strategy_two(maze, fire, start, goal, q):
         temp_start = (curX, curY)
 
         fire_maze = advance_fire_one_step(fire_maze, q)
-        #print (fire_maze)
         temp_maze = reset_path(fire_maze, dimension) # avoid backtracking unless necessary
 
-    print('Escaped')
-    return fire_maze
-'''
-def strategy_three (maze, fire, start, goal, q):
-    fire_maze = np.copy(fire)
-    future_fire_maze = np.copy(fire_maze)
-    dimensions = maze.shape[0]
-    probability_maze = init_probability_maze(fire_maze, dimensions)
+    return 'Escaped', fire_maze
 
-    curX = 0
-    curY = 0
-    temp_start = (curX, curY)
-    temp_maze = np.copy(fire_maze)
-
-    status, traversal_path, num_explored_nodes = bfs(temp_maze, temp_start, goal)
-
-    if status == 'No Solution':
-        print('No path possible')
-        return fire_maze
-
-    while fire_maze[curX][curY] != 4:
-        if fire_maze[curX][curY] == 7:
-            print('Died')
-            print(str(curX) + ', ' + str(curY))
-            return fire_maze
-        
-        oldX = curX
-        oldY = curY
-
-        if curX + 1 < dimensions and (traversal_path[curX + 1][curY] == 6 or traversal_path[curX + 1][curY] == 4):
-            curX = curX + 1
-        elif curY + 1 < dimensions and (traversal_path[curX][curY + 1] == 6 or traversal_path[curX][curY + 1] == 4):
-            curY = curY + 1
-        elif curX - 1 >= 0 and (traversal_path[curX - 1][curY] == 6 or traversal_path[curX - 1][curY] == 4):
-            curX = curX - 1
-        elif curY - 1 >= 0 and (traversal_path[curX][curY - 1 ] == 6 or traversal_path[curX][curY - 1] == 4):
-            curY = curY - 1
-        
-        if fire_maze[curX][curY] == 4:
-            break
-
-        probability_maze = calculate_fire_probability(probability_maze, fire_maze, dimensions, q)
-        print(probability_maze)
-        
-        #if random() <= (1 - probability_maze[curX][curY]): # probability that the cell will not be on fire
-        #    fire_maze[curX][curY] = 6
-        #    fire_maze = advance_fire_one_step(fire_maze, q)
-        #    #print(fire_maze)
-        #    #print('continue')
-        #    continue
-        
-        if probability_maze[curX][curY] == 0:
-            fire_maze[curX][curY] = 6
-            fire_maze = advance_fire_one_step(fire_maze, q)
-            
-        elif probability_maze[curX][curY] >= 0:
-            temp_start = (oldX, oldY)
-            fire_maze[curX][curY] = 8 # avoided because of fire prediction
-            curX, curY = oldX, oldY
-            temp_maze = reset_path(fire_maze, dimensions)
-            status, traversal_path, num_explored_nodes = bfs(temp_maze, temp_start, goal)
-
-            if status == 'No Solution':
-                print('No path possible')
-                return fire_maze
-            #print('find new path')
-            #print(fire_maze)
-
-    print('Escaped')
-    return fire_maze
-'''
 
 def strategy_three(maze, fire, start, goal, q):
     dimensions = maze.shape[0]
@@ -203,15 +129,12 @@ def strategy_three(maze, fire, start, goal, q):
     status, traversal_path = astar_fire(fire_maze, start, goal, probability_maze)
 
     if status == 'No Solution':
-        print('No path possible')
-        return fire_maze
+        return 'No Path Possible', fire_maze
 
     while fire_maze[curX][curY] != 4:
 
         if fire_maze[curX][curY] == 7:
-            print('Died')
-            print(str(curX) + ', ' + str(curY))
-            return fire_maze
+            return 'Died', fire_maze
 
 
         if curX + 1 < dimensions and (traversal_path[curX + 1][curY] == 6 or traversal_path[curX + 1][curY] == 4):
@@ -225,6 +148,7 @@ def strategy_three(maze, fire, start, goal, q):
         
         if fire_maze[curX][curY] == 4:
             break
+
         fire_maze[curX][curY] = 6
         current = (curX, curY)
         n = find_time_steps(current, goal)
@@ -235,14 +159,9 @@ def strategy_three(maze, fire, start, goal, q):
         status, traversal_path = astar_fire(temp_maze, current, goal, probability_maze)
 
         if status == 'No Solution':
-            print('No path Possible')
-            return fire_maze
+            return 'No Path Possible', fire_maze
 
-        #print(status)
-        #print(probability_maze)
-
-    print('Escaped')
-    return fire_maze
+    return 'Escaped', fire_maze
 
 
 def init_probability_maze (fire_maze, dimensions):
@@ -253,32 +172,6 @@ def init_probability_maze (fire_maze, dimensions):
                 probability_maze[x][y] = 1
     
     return probability_maze
-
-'''
-def calculate_fire_probability(probability_maze, fire_maze, dimensions, q):
-    maze_copy = np.copy(probability_maze)
-    for x in range(0, dimensions):
-        for y in range(0, dimensions):
-            if fire_maze[x][y] == 7:
-                maze_copy[x][y] = 1
-            elif (fire_maze[x][y] != 1 or fire_maze[x][y] != 3 or fire_maze[x][y] != 4):
-                k = 0
-                if x - 1 >= 0:
-                    if fire_maze[x - 1][y] == 7:
-                        k += 1
-                if x + 1 < dimensions:
-                    if fire_maze[x + 1][y] == 7:
-                        k += 1
-                if y - 1 >= 0:
-                    if fire_maze[x][y - 1] == 7:
-                        k += 1
-                if y + 1 < dimensions:
-                    if fire_maze[x][y + 1] == 7:
-                        k += 1
-                prob = 1 - (1 - q) ** k
-                maze_copy[x][y] = prob
-    return maze_copy
-''' 
 
 def calculate_fire_probability(probability_maze, fire_maze, dimensions, q, n):
     old_prob_maze = np.copy(probability_maze)
@@ -316,6 +209,4 @@ def reset_path(maze, dimension):
     return temp_maze
 
 def find_time_steps(current, goal):
-    return np.ceil( euclidean_distance(current, goal) / 5 )
-   
-
+    return np.ceil( euclidean_distance(current, goal) / 5)
--- main.py
+++ main.py
@@ -9,14 +9,65 @@ from bfs import *
 from astar import *
 from fire import *
 
-## white: free, black: wall, purple: start/goal, green: optimal path, blue: all visited, red: fire
+# white: free, black: wall, purple: start/goal, green: optimal path, blue: all visited, red: fire
 color_set = ['white', 'black', 'purple', 'blue', 'green', 'red', 'pink']
 range_set = np.array([-0.5, 0.5, 2.5, 4.5, 5.5, 6.5, 7.5, 8.5])
 
 def main():
-    test_fire_strategies()
-    #test_path_algorithms()
+    #test_fire_strategies()
+    test_path_algorithms()
+    #run_strat_trials()
 
+def run_strat_trials():
+    dimension = int(sys.argv[1])
+    density = float(sys.argv[2])
+    flammability = float(sys.argv[3])
+    strategy = sys.argv[4]
+    cmap = colors.ListedColormap(color_set)
+    norm = colors.BoundaryNorm(range_set, len(color_set))
+    plt.figure(figsize = (8, 8))
+    plt.axis('off')
+
+    mazeTrials = 0
+    totalSuccesses = 0
+
+    while mazeTrials < 10:
+        maze = generate_maze(dimension, density)
+
+        status, trial_maze = dfs(maze, (0, 0), (dimension - 1, dimension - 1))
+
+        while status != 'Success':
+            maze = generate_maze(dimension, density)
+            status, trial_maze = dfs(maze, (0, 0), (dimension - 1, dimension - 1))
+        
+        currentIterSuccesses = 0
+        fireTrials = 0
+
+        while fireTrials < 10:
+            fire_maze, fireCoord = init_fire(maze)
+            status, trial_maze = dfs(fire_maze, (0, 0), fireCoord)
+
+            while status != 'Success':
+                fire_maze, fireCoord = init_fire(maze)
+                status, trial_maze = dfs(fire_maze, (0, 0), fireCoord)
+
+            if strategy == 's1':
+                status, fire_result = strategy_one(maze, fire_maze, (0, 0), (dimension - 1, dimension - 1), flammability)
+            elif strategy == 's2':
+                status, fire_result = strategy_two(maze, fire_maze, (0, 0), (dimension - 1, dimension - 1), flammability)
+            elif strategy == 's3':
+                status, fire_result = strategy_three(maze, fire_maze, (0, 0), (dimension - 1, dimension - 1), flammability)
+
+            if status == 'Escaped':
+                currentIterSuccesses += 1
+        
+            fireTrials += 1
+        
+        totalSuccesses += currentIterSuccesses
+        mazeTrials += 1
+    
+    print(totalSuccesses)
+    
 def test_fire_strategies():
     cmap = colors.ListedColormap(color_set)
     norm = colors.BoundaryNorm(range_set, len(color_set))
@@ -25,31 +76,23 @@ def test_fire_strategies():
 
     dimension = int(sys.argv[1])
     density = float(sys.argv[2])
+    flammability = float(sys.argv[3])
 
     maze = generate_maze(dimension, density)
-    fire_maze = init_fire(maze)
+    fire_maze, fireCoord = init_fire(maze)
 
-    print(maze)
-    print(fire_maze)
-    #fire_result = strategy_one(maze, fire_maze, (0, 0), (dimension - 1, dimension - 1), 0.3)
-    #fire_result2 =  strategy_two(maze, fire_maze, (0,0), (dimension - 1, dimension - 1), 0.3)
-    #status, bfs_result, num = bfs(fire_maze, (0, 0), (dimension - 1, dimension - 1))
-    fire_result3 =  strategy_three(maze, fire_maze, (0,0), (dimension - 1, dimension - 1), 0.3)
+    status, fire_result = strategy_one(maze, fire_maze, (0, 0), (dimension - 1, dimension - 1), flammability)
+    status, fire_result2 =  strategy_two(maze, fire_maze, (0,0), (dimension - 1, dimension - 1), 0.3)
+    status, fire_result3 =  strategy_three(maze, fire_maze, (0,0), (dimension - 1, dimension - 1), flammability)
 
+    plt.imshow(fire_result, cmap = cmap, norm = norm)
+    plt.savefig('fire_strat1.jpg')
 
-    #plt.imshow(fire_result, cmap = cmap, norm = norm)
-    #plt.savefig('fire_strat1.jpg')
-
-    #plt.imshow(fire_result2, cmap = cmap, norm = norm)
-    #plt.savefig('fire_strat2.jpg')
-
-    #plt.imshow(bfs_result, cmap = cmap, norm = norm)
-    #plt.savefig('bfs_result.jpg')
+    plt.imshow(fire_result2, cmap = cmap, norm = norm)
+    plt.savefig('fire_strat2.jpg')
 
     plt.imshow(fire_result3, cmap = cmap, norm = norm)
     plt.savefig('fire_strat3.jpg')
-
-
 
 def test_path_algorithms():
     dimension = int(sys.argv[1])
@@ -62,40 +105,44 @@ def test_path_algorithms():
 
     maze = generate_maze(dimension, density)
 
-    prob_success = success_chance(dimension, density)
-    print('Probabilty S can be reached from G is: ' + str(prob_success))
+    #prob_success = success_chance(dimension, density)
+    #print('Probabilty S can be reached from G is: ' + str(prob_success))
 
-    diff_explored = difference_explored(dimension, density)
-    print('Average difference explored between BFS and A* is: ' + str(diff_explored))
-
+    #diff_explored = difference_explored(dimension, density)
+    #print('Average difference explored between BFS and A* is: ' + str(diff_explored))
+    
     start_time = time.time()
     status, astar_maze, num_explored_nodes = astar(maze, (0, 0), (dimension - 1, dimension - 1))
     end_time = time.time()
     print(status)
     print(end_time - start_time)
     print(num_explored_nodes)
-
+    
+    
     start_time = time.time()
     status, bfs_maze, num_explored_nodes = bfs(maze, (0, 0), (dimension - 1, dimension - 1))
     end_time = time.time()
     print(status)
     print(end_time - start_time)
     print(num_explored_nodes)
-
+    
     start_time = time.time()
     status, dfs_maze = dfs(maze, (0, 0), (dimension - 1, dimension - 1))
     end_time = time.time()
     print(status)
     print(end_time - start_time)
-
+    
+    
     plt.imshow(astar_maze, cmap = cmap, norm = norm)
     plt.savefig('astar_maze.jpg')
-
+    
     plt.imshow(bfs_maze, cmap = cmap, norm = norm)
     plt.savefig('bfs_maze')
-
+    
     plt.imshow(dfs_maze, cmap = cmap, norm = norm)
     plt.savefig('dfs_maze')
+    
+    
 
 def generate_maze(dimension, density):
     maze = np.zeros((dimension, dimension), int)
@@ -131,4 +178,6 @@ def difference_explored(dimension, density):
         total_difference += (num_explored_bfs - num_explored_astar)
     
     return (total_difference / 100)
-main()
+
+if __name__ == '__main__':
+    main()
